9bb4bc9f91294524906e0dfb49a4babb3c6dd71c
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-07 12:55:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,7 @@
 
 /**
  * main - Entry point
- *
  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
  * Return: Always 0 (Success)
  */
 
@@ -13,3 +11,4 @@ int main(void)
 {
 		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
+}
