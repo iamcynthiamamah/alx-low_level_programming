17557cbc3c76699797cdecffc13cc5cab991a392
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-09 13:44:31 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always return 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= z)
+	{
+		if (ch != 'e' || ch != 'q')
+		{
+		putchar(ch);
+		ch++;
+		}
+		putchar('\n');
+	}
+	return (0);
+
+}
