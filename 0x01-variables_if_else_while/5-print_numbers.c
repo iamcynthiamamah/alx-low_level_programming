36813d3682d7e2651e95154d46e1a0628a232342
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-09 23:24:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always return 0 (successful)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 1; n < 10; n++)
+	{
+		printf('n');
+	}
+
+	printf('\n');
+	return (0);
+
+}
