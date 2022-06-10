e4c4f3a60167fd0e145ba7efcdf41200bdc82d11
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-10 03:21:24 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 57; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
+		if (n != 57)
+		{
 		putchar(44);
 		putchar(32);
+		}
 	}
-	putchar(57,'\n');
+	putchar('\n');
 	return (0);
 }
