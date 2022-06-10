8a6f81f7e0e2aa709b12e9d9137a361364d930ac
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-10 01:35:58 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	char n;
+	int n;
 	char ch;
 
 	for (n = 48; n <= 58; n++)
 	{
 		putchar(n);
 	}
-		for (ch = 'a'; ch = 'f'; ch++)
+	for (ch = 'a'; ch = 'f'; ch++)
 	{
 		putchar(ch);
 	}
