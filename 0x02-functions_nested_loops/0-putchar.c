4389f6097f8d6d96e506da909df4c9197028c5c4
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-10 07:24:02 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always return 0 (success)
+ */
+
+int main(void)
+{
+	char ch = _putchar;
+
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
