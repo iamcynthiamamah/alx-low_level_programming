57c01c119041d36084636dcb3a16b73daceb29d7
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-10 01:16:11 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always returns 0 (success)
+ */
+
+int main(void)
+{
+	char n;
+	char ch;
+
+	for (n = 48; n <= 58; n++)
+	{
+		putchar(n);
+	}
+		for (ch = 'a'; ch = 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
