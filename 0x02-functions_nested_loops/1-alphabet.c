d12f673d777ce06b9a9f4501e665bb6685ac3eef
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-10 16:15:13 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * main - Entry point
+ *
+ * Return: always return 0 (success)
+ */
+
+void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+}
+
+int main(void)
+{
+	 print_alphabet();
+	return (0);
+}
+
