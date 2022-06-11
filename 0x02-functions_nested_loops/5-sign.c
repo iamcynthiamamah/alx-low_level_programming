3fabfe2257b200be8ca004217c41b6e82b08dfbf
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-11 11:51:08 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks code in lowercase
+ * @n: basics
+ * Return: Always 0
+ */
+
+int print_sign(int n)
+{
+	int print_sign(int n);
+
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
