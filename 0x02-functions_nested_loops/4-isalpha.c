f570821ba43a556c3bd700bd7684e3436823dd25
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-11 11:36:16 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints alpha
+ * @c: Description
+ * Return: Always return 0
+ */
+
+int _isalpha(int c)
+{
+	int isalpha(int c);
+
+	if isalpha(int c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
