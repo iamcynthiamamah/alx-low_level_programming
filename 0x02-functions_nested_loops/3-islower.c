cd0157bbcd1652df0502123404f05d2b66894d30
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-11 08:30:10 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Checks code in lowercase
+ *
+ * @c: basics
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
