ef281d3484cd707deda859fd8a244730e43b3ca8
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-12 14:50:05 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,31 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes there */
+#include "main.h"
+
 /**
- *  *main - Entry point
- *   *
- *    *Return: always return 0 (successful)
- *     */
-/* betty style doc for function main goes there */
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
 int main(void)
 {
-		int n;
+	int i;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-				/* your code goes there */
-					if (n > 0)
-							{
-										printf("%d is positive\n", n);
-											}
-						else if (n == 0)
-								{
-											printf("%d is zero\n", n);
-												}
-							else
-									{
-												printf("%d is negative\n", n);
-													}
-								return (0);
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
