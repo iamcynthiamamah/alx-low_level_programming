d0c3d111e8c738b8d5f0349c8333c8bb68aac571
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-12 17:42:29 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * 
+ */
+
+void times_table(void)
+{
+	int x;
+	
+	int y;
+	
+	int z;
+	
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			
+
