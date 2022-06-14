ffb6e1026e71c1ea645c90f3cfc6e46a934eee6b
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-14 16:03:17 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - puts
+ *
+ * return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	while (str[1]);
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
