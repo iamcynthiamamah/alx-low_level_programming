8c62576599229728fe78a49d07dbc90f8ba01e1f
iamcynthiamamah <cynthiamamah@gmail.com>
2022-06-14 16:55:47 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - puts
- *
- * return: Always 0
+ * _puts - prints string
+ * @str: string
+ * Return: void
  */
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[1] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
-
 	_putchar('\n');
 }
